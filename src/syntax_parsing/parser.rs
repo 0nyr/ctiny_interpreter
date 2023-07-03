71acf6f6970a4ae3cbcec44664b0a8e030014be4
0nyr <rascoussier.florian@gmail.com>
2023-07-03 13:30:57 +0200

    Trying to work with PEG grammar, written first test.

--- src/syntax_parsing/parser.rs
+++ src/syntax_parsing/parser.rs
@@ -6,6 +6,24 @@ use pest::Parser;
 #[grammar = "ctiny.pest"]
 pub struct CTinyParser;
 
+#[test]
+fn my_test() {
+    // to see test output: $ cargo test -- --nocapture
+    let parse_result = CTinyParser::parse(Rule::addition, "1773 + 1362").unwrap();
+    
+    // check that the first pair is an the correct addition
+    let pair = parse_result.clone().next().unwrap();
+    assert_eq!(pair.as_rule(), Rule::addition);
+    assert_eq!(pair.as_str(), "1773 + 1362");
+    
+    // print the tokens
+    let tokens = parse_result.tokens();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
 pub fn parse(file_content: String) -> Result<(), pest::error::Error<Rule>> {
     log::debug!("Content of file: {}", file_content);
 
